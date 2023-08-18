36da94d3187e4252753a8b764b07a0d3e5607577
Kessia-Milena <kessia.milena124@yahoo.com>
2023-08-17 22:00:51 -0300

    Inclusão de dados ListaServiços da API

--- src/pages/Servicos/index.tsx
+++ src/pages/Servicos/index.tsx
@@ -4,88 +4,87 @@ import Logo from "../../assets/img/logo.svg"
 import Facebook from "../../assets/img/facebook.svg"
 import Instagram from "../../assets/img/instagram.svg"
 import Linkedin from "../../assets/img/linkedin.svg"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import CardServ from "../../components/CardServ"
+
+import api from "../../utils/api";
 
 function Servicos() {
 
     const [servs, setServ] = useState<any[]>([
-        {
-            titulo: "Desenvolvimento de site institucional - Gateway de Pagamento",
-            valor: "R$ 1300,00",
-            descricao: "Desenvolver um site responsivo que seja utilizado como uma plataforma de apresentação do nosso gateway de pagamento. O objetivo principal deste projeto é criar um site atraente e informativo, que demonstre as funcionalidades e benefícios do nosso gateway de pagamento para potenciais clientes.",
-            techs: ["HTML", "CSS", "REACT"]
-        },
-        {
-            titulo: "Caixa Rápido",
-            valor: "R$ 1200,00",
-            descricao: "Preciso fazer um código em python para um bot do telegram. O bot será para solicitação de pagamento.",
-            techs: ["PYTHON"]
-        },
-        {
-            titulo: "Bot telegram Pagamento",
-            valor: "R$ 2400,00",
-            descricao: "Preciso fazer um  software que permita ao usuário fazer o upload de seu extrato bancário em formato( ofx). Dentro do software o mesmo poderá categorizar todas as suas receitas e despesas, tendo categorias sugeridas pelo software e permitindo também personalizações. Após o lançamento de vários extratos o software irá entender que são lançamentos parecidos e fará a categorização de maneira automática, cabendo ao usuário somente categorizar as receitas e despesas que não se repetem. Após a categorização o software irá emitir gráficos e relatórios baseados na categorização das contas.",
-            techs: ["PYTHON"]
-        },
+
     ]);
 
     const [skillDigitada, setSkillDigitada] = useState<string>("");
-    const [listaServFiltrados, setListaServFiltrados] = useState<any[]>(servs)
+    const [servicos, setServicos] = useState<any[]>(servs)
+
+    useEffect(() => {
+        document.title = "Lista de Serviços - VSConnect"
+
+        listarServicos()
+    }, [])
 
     function buscarPorSkill(event: any) {
         event.preventDefault();
 
         const servFiltrados = servs.filter((serv: any) => serv.techs.includes(skillDigitada.toLocaleUpperCase()));
 
-        if( servFiltrados.length === 0 ) {
+        if (servFiltrados.length === 0) {
             alert("Não existem serviços com a skill informada.")
         } else {
-            setListaServFiltrados(servFiltrados);
+            setServicos(servFiltrados);
         }
     }
 
     function retornoServicos(event: any) {
 
-        if( event.target.value === "" ) {
-            setListaServFiltrados(servs);
-        } 
+        if (event.target.value === "") {
+            listarServicos()
+        }
 
         setSkillDigitada(event.target.value);
     }
 
+    function listarServicos() {
 
-    return(
+        api.get("servicos").then((response: any) => {
+            console.log(response.data)
+            setServicos(response.data)
+        }).catch((error: any) => console.log(error))
+    }
+
+
+    return (
         <>
             <div id="sombra"></div>
             <main id="lista-servicos">
                 <div className="container container_lista_servicos">
                     <div className="lista_servicos_conteudo">
                         <h1>Lista de Serviços</h1>
-                        <hr/>
-                        <form method="post" onSubmit={ buscarPorSkill }>
+                        <hr />
+                        <form method="post" onSubmit={buscarPorSkill}>
                             <div className="wrapper_form">
                                 <label htmlFor="busca">Procurar serviços</label>
                                 <div className="campo-label">
-                                    <input type="search" name="campo-busca" id="busca" placeholder="Buscar serviços por tecnologias..." onChange={ retornoServicos }/>
+                                    <input type="search" name="campo-busca" id="busca" placeholder="Buscar serviços por tecnologias..." onChange={retornoServicos} />
                                     <button type="submit">Buscar</button>
                                 </div>
                             </div>
                         </form>
                         <div className="wrapper_lista">
-                            <ul>     
-                                {listaServFiltrados.map((serv: any, index: number) => {
+                            <ul>
+                                {servicos.map((serv: any, index: number) => {
                                     return <li key={index}>
                                         <CardServ
-                                        titulo={serv.titulo}
-                                        valor={serv.valor}
-                                        descricao={serv.descricao}
-                                        techs={serv.techs}
+                                            titulo={serv.nome}
+                                            valor={serv.valor}
+                                            descricao={serv.descricao}
+                                            techs={serv.techs}
                                         />
 
                                     </li>
-                                })}                           
+                                })}
                             </ul>
                         </div>
                     </div>
@@ -104,13 +103,13 @@ function Servicos() {
                                 <li>Cadastrar Desenvolvedor</li>
                             </ul>
                         </div>
-                        <img src= { Logo } alt=""/>
+                        <img src={Logo} alt="" />
                         <div className="rodape_conteudo_contatos">
                             <h2>Contatos</h2>
                             <div>
-                                <a href="#"><img src= { Facebook } alt=""/></a>
-                                <a href="#"><img src= { Instagram } alt=""/></a>
-                                <a href="#"><img src= { Linkedin } alt=""/></a>
+                                <a href="#"><img src={Facebook} alt="" /></a>
+                                <a href="#"><img src={Instagram} alt="" /></a>
+                                <a href="#"><img src={Linkedin} alt="" /></a>
                             </div>
                             <a href="mailto:">contato@vsconnect.com</a>
                         </div>
